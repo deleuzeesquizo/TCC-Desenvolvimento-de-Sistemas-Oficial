67a5f338bcef4fd19f1c3c8f8ec98d8d22f44133
Leonardo Rodrigues de Carvalho <177953595+deleuzeesquizo@users.noreply.github.com>
2025-03-15 18:36:55 -0300

    commitando um erro

--- Projeto/src/llm/trainer.py
+++ Projeto/src/llm/trainer.py
@@ -4,19 +4,6 @@ import logging
 from transformers import AutoModelForCausalLM, AutoTokenizer, Trainer, TrainingArguments
 from dataset import load_dataset
 
-#Criar uma classe que vai agrupar as funções essenciais para criação do modelo LLM 
-class ModelConstruct:
-    #Vai iniciar o modelo 
-    def _init_(self, num_layers, num_heads, vocab_size, d_model):
-        self.modelName = DS_TCC
-        #variavel para o numero de camadas 
-        self.num_layers = 
-        #
-        self.num_heads = 
-        self.vocab_size = 
-        self.d_model = 
-        self.d_ff =
-        self.dropout_rate =
 class SimpleCausaLlm(nn.Module):
     def __init__(self):
     def embedding(self):
